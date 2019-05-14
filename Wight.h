bc5bf78531c1e5d5fd1a3bfc8a03dcccf0fb3151
sean2695 <34174434+sean2695@users.noreply.github.com>
2019-05-14 12:46:28 -0400

    Wight.h

---
+++ Wight.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include "Undead.h"
+
+
+
+
+
+class Wight : public Undead
+
+{
+
+public:
+
+	//constuctor 
+
+	Wight(std::string name);
+
+	//function to call to attack
+
+	void attack();
+
+
+
+};
